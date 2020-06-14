9e9a5cb7df3e1d63c7e5e221f489d44572d649e1
Khalid-ITGDP <kalreqabi@gmail.com>
2020-06-15 02:35:55 +0300

    Changed restart question

--- bikeshare.py
+++ bikeshare.py
@@ -245,7 +245,7 @@ def main():
         user_stats(df)
         data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart? Enter yes.\n')
         if restart.lower() != 'yes':
             break
 
